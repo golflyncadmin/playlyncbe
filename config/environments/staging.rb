3605cd3afb9db455789f7e00ea820fa69dce8c01
fahad butt <fahad.butt@theappguys.com>
2024-09-05 21:19:12 +0500

    staging_host_update

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.hosts << "https://staging.playlync.com/"
+  Rails.application.routes.default_url_options = staging.playlync.com
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
