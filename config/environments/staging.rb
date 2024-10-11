c52f8f88f4af132c8105c5e32160f1d898d3b478
fahad butt <fahad.butt@theappguys.com>
2024-10-11 18:19:00 +0500

    fix_forgot_password_mailer_issue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'staging.playlync.com', protocol: 'https' }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
