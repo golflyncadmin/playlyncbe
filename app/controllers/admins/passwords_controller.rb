b7a1cc2cf3e402104c810bbf285c09ffadb40892
Asad <Asad.I@theappguys.com>
2024-10-11 20:51:19 +0500

    fix reset password link issue

--- app/controllers/admins/passwords_controller.rb
+++ app/controllers/admins/passwords_controller.rb
@@ -4,7 +4,7 @@ class Admins::PasswordsController < Devise::PasswordsController
     if admin.present?
       token = admin.send_reset_password_instructions
       otp_service = OtpService.new(admin)
-      otp_service.send_reset_password_email(admin.email, edit_password_url(admin, reset_password_token: token))
+      otp_service.send_reset_password_email(admin.email, edit_admin_password_url(reset_password_token: token, host: request.base_url))
       flash[:notice] = 'Password recovery instructions sent.'
       redirect_to admins_password_reset_path(resource_name, email: admin.email)
     else
@@ -15,13 +15,7 @@ class Admins::PasswordsController < Devise::PasswordsController
 
   def update
     self.resource = resource_class.reset_password_by_token(resource_params)
-
-    if resource.valid_password?(resource_params[:password])
-      flash[:alert] = "You cannot reuse your previous password. Please choose a different one."
-      redirect_to edit_admin_password_path(reset_password_token: resource.reset_password_token)
-      return
-    end
-
+ 
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
       flash[:notice] = 'Password has been changed successfully.'
@@ -37,7 +31,7 @@ class Admins::PasswordsController < Devise::PasswordsController
     if admin
       token = admin.send_reset_password_instructions
       otp_service = OtpService.new(admin)
-      otp_service.send_reset_password_email(admin.email, edit_password_url(admin, reset_password_token: token))
+      otp_service.send_reset_password_email(admin.email, edit_admin_password_url(reset_password_token: token, host: request.base_url))
       flash[:notice] = 'Reset password instructions resent.'
       redirect_to admins_password_reset_path(resource_name, email: admin.email)
     else
@@ -45,6 +39,13 @@ class Admins::PasswordsController < Devise::PasswordsController
       redirect_back(fallback_location: new_session_path(resource_name))
     end
   end
+
   def reset
+  end
+ 
+  private
+ 
+  def assert_reset_token_passed
+    true
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,5 +4,11 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :full_name, presence: true
+  validates :full_name, presence: true,
+                        format: { with: /\A[A-Za-z][A-Za-z\s]*\z/, message: "must start with letter" },
+                        length: { maximum: 25 }
+
+	def send_reset_password_instructions
+		set_reset_password_token
+	end
 end
